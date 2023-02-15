f748e26fc1a2788ce6d13e166f87ce38da6d5b80
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2023-02-15 00:59:45 -0800

    add final iteration of button isplay

--- gecko/utils.py
+++ gecko/utils.py
@@ -273,5 +273,43 @@ def plot_data(df) -> None:
         else:
             return px.scatter(df, x=x, y=y, color = "Group", template = "seaborn", title = title, width = 800, height = 800)
 
-def feedback_button() -> HTML:
-    return HTML(filename="button.html")
+def feedback_button() -> None:
+    button = '''
+    <style>
+    .button {
+        border: 0;
+        padding: 16px 32px 16px 32px;
+        text-align: center;
+        font-size: 18px;
+        display: flex;
+        transition: color 5s, 
+                    background 5s, 
+                    box-shadow 1s ease-out;
+        cursor: pointer;
+        color: #003262;
+        border-radius: 30px;
+        margin: 10% auto;
+        background: linear-gradient(145deg, #e2e2e2, #bebebe);
+        box-shadow: 20px 20px 40px 10px #afafaf, 
+                    -20px -20px 40px 10px #f7f7f7, 
+                    inset 0 0 0px, 
+                    inset 0 0 0px;
+    }
+    
+    .button:hover {
+        color: #D3D3D3;
+        background: #003262;
+        box-shadow: 0 0 0px #000000, 
+                    0 0 0px #000000, 
+                    inset 20px 20px 40px 10px #002a51, 
+                    inset -20px -20px 40px 10px #003b73;
+    }
+    </style>
+
+    <a href="https://forms.gle/hipxf2uFw5Ud4Hyn8">
+        <button class="button">
+            Fill out the feedback form here
+        </button>
+    </a>
+    '''
+    display(HTML(button))
