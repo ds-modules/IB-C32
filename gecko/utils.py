0443340f7893ef3d6cd9d81ea03b9a8a69c701a2
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2023-02-14 21:15:08 -0800

    make decorator for instructions

--- gecko/utils.py
+++ gecko/utils.py
@@ -1,15 +1,51 @@
-from datascience import *
 from IPython.display import *
 from ipywidgets import *
 import pandas as pd
-import numpy as np
 import plotly.express as px;
-import scipy.constants as constant 
-import math
 from ipywidgets import *
 import re
 import json
 from typing import *
+
+class Instructions:
+    
+    def index(fn):
+        if fn == "enter_team_number":
+            return Instructions.team
+        elif fn == "show_data":
+            return Instructions.data
+        elif fn == "plot_data":
+            return Instructions.plot
+        else:
+            print("No instructions found for this function.")
+    
+    def team():
+        show("Select a team number to get data for.", tags=["h2"])
+        show("You can also use the arrow keys to change the team number, or simply type it into the box.", tags=["blockquote", "h3"])
+
+    def data():
+        show("Use the dropdown menu to select a group to view.", tags=["h2"])
+        show("Then, use the slider to determine how many rows to show.", tags=["h3"])
+        show("Note: You can also click the number next to the slider to change the number of rows.", tags=["blockquote", "h3"])
+        show("Make sure you take a screenshot of the \"My Team\" table to submit!", tags=["h1", "center"])
+    
+    def plot():
+        show("Use the dropdown menu to select a column to plot on the x-axis and y-axis.", tags=["h2"])
+        show("Then, use the checkbox to toggle trend lines.", tags=["h3"])
+        show("Make sure to save the graph as an image to submit!", tags=["h1", "center"])
+        show("To do that, hover over the top right corner of the graph and click the camera button!", tags=["h2", "center"])
+        
+def show_instructions(func):
+    fn = name(func)
+    instruct = Instructions.index(fn)
+    def inner(*args, **kwargs):
+        instruct()
+        return func(*args, **kwargs)
+    return inner
+
+def name(func):
+    assert isinstance(func, Callable), "Input must be callable."
+    return func.__name__
 
 def read(fp: str, full_fp: bool = False) -> dict:
     """
@@ -161,9 +197,8 @@ def get_class_data(team : int, section : int) -> pd.DataFrame:
     df["Group"] = groups
     return df
     
+@show_instructions    
 def enter_team_number():
-    show("Select a team number to get data for.", tags=["h2"])
-    show("You can also use the arrow keys to change the team number, or simply type it into the box.", tags=["blockquote", "h3"])
     w = BoundedIntText(value = None, min = 1, max = 35, step = 1, description = "Team:")
     display(w)
     return w
@@ -187,14 +222,11 @@ def get_info(w : widgets.widget_int.BoundedIntText) -> Tuple[int, int]:
     section = section_from_team(team)
     return (team, section)
 
+@show_instructions
 def show_data(data : pd.DataFrame) -> None:
     section = data.query("Group == 'My Team'")["Section"].values[0]
     team = data.query("Group == 'My Team'")["Team"].values[0]
     data = data.copy().drop(columns=["Section", "Team"])
-    show("Use the dropdown menu to select a group to view.", tags=["h2"])
-    show("Then, use the slider to determine how many rows to show.", tags=["h3"])
-    show("Note: You can also click the number next to the slider to change the number of rows.", tags=["blockquote", "h3"])
-    show("Make sure you take a screenshot of the \"My Team\" table to submit!", tags=["h1", "center"])
     @interact(group = Dropdown(value = "My Team", options = ["My Team", "My Section", "Rest of Class"]),
               rows = IntSlider(value = 10, min = 1, max = 60, step = 1),
               section = fixed(section),
@@ -228,13 +260,16 @@ def get_tables(widget) -> Tuple[int, int, pd.DataFrame]:
     data = get_class_data(team, section)
     return (team, section, data)   
 
+@show_instructions
 def plot_data(df):
     cols = ["Mass (g)", "Angle (degrees)", "Shear Force (N)", "Adhesive Force (N)"]
-    show("Use the dropdown menu to select a column to plot on the x-axis and y-axis.", tags=["h2"])
-    show("Make sure to save the graph as an image to submit!", tags=["h1", "center"])
-    show("To do that, hover over the top right corner of the graph and click the camera button!", tags=["h2", "center"])
     @interact(x = Dropdown(value = "Mass (g)", options = cols),
-              y = Dropdown(value = "Angle (degrees)", options = cols))
-    def helper(x, y):
+              y = Dropdown(value = "Angle (degrees)", options = cols),
+              lines = Checkbox(value = False, description = "Show Trend Lines")
+    )
+    def helper(x, y, lines):
         title = f"{x} vs {y}"
-        return px.scatter(df, x=x, y=y, color = "Group", template = "seaborn", title = title, width = 800, height = 800, trendline = "ols")
+        if lines:
+            return px.scatter(df, x=x, y=y, color = "Group", template = "seaborn", title = title, width = 800, height = 800, trendline = "ols")
+        else:
+            return px.scatter(df, x=x, y=y, color = "Group", template = "seaborn", title = title, width = 800, height = 800)
